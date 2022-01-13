519fdeb29d2a6ad801ee975244db9c1d24087ed2
Izabelly Brito <izabelly.rb@hotmail.com>
2022-01-12 23:37:07 -0300

    final adjustments + media queries

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-var listI = document.getElementsByClassName('elementaryI')
+/**/
+var listes = document.querySelector('.elementaryS');
+var listms =  document.querySelector('.middleS')
+var img = document.querySelector('.kids1')
+var img2 = document.querySelector('.kids2')
 
 function show() {
-    ;
+    listes.classList.toggle('on')
+    img.classList.toggle('on')
+    listms.classList.remove('on')
+    img2.classList.remove('on')
 }
+
+function show2() {
+    listms.classList.toggle('on')
+    img2.classList.toggle('on')
+    listes.classList.remove('on')
+    img.classList.remove('on')
+}
+
